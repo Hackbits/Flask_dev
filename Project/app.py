1f67f2f10aaa4aa6d7e018e5841cb32afb43e85a
Hackbits <sridharrao764@gmail.com>
2025-10-16 10:16:36 +0530

    Minor redirect Changes

--- Project/app.py
+++ Project/app.py
@@ -34,7 +34,7 @@ def login():
         password = request.form.get("password")
         if username == "admin" and password == "12345678":
             session["user"] = username
-            return redirect(url_for("home"))
+            return redirect("/")
         else:
             return render_template("login.html", error="Invalid credentials")
     return render_template("login.html")
