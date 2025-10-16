05024e51f2675bfdb5fd7ded815722f9f6f1dec4
Hackbits <sridharrao764@gmail.com>
2025-10-16 10:59:46 +0530

    Minor redirct Changes

--- Project/app.py
+++ Project/app.py
@@ -28,13 +28,12 @@ def for_developers():
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
-    return render_template("login.html")
     if request.method == "POST":
-        username = request.form.get("username")
+        username = request.form.get("username") 
         password = request.form.get("password")
         if username == "admin" and password == "12345678":
             session["user"] = username
-            return redirect("/")
+            return redirect(url_for("home"))
         else:
             return render_template("login.html", error="Invalid credentials")
     return render_template("login.html")
